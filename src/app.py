d02cd91ec050a31b9c709e917538dfc43cb5fa6e
MdTanjeemHaider <mdtanjeem.haider@ontariotechu.net>
2024-11-27 19:49:19 -0500

    Implement Error Handling

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+# Imports
 from datetime import datetime
 from flask import Flask, jsonify, redirect, render_template, request, session, url_for
 from dotenv import load_dotenv
@@ -61,113 +62,132 @@ def loginregister():
 # Login route
 @app.route('/login', methods=['POST'])
 def login():
-    # Extract user data from request
-    data = request.form
-    email = data['email']
-    password = data['password']
+    try:
+        # Extract user data from request
+        data = request.form
+        email = data['email']
+        password = data['password']
 
-    # Authenticate user
-    user = auth.sign_in_with_email_and_password(email, password)
+        # Authenticate user
+        user = auth.sign_in_with_email_and_password(email, password)
 
-    # Store user in session
-    session['uuid'] = user['localId']
-    session['email'] = email
-    session['token'] = user['idToken']
+        # Store user in session
+        session['uuid'] = user['localId']
+        session['email'] = email
+        session['token'] = user['idToken']
 
-    # Check if user is in database, if not add them
-    if not User.query.filter_by(id=session['uuid']).first():
-        new_user = User(id=session['uuid'], email=session['email'])
-        db.session.add(new_user)
-        db.session.commit()
+        # Check if user is in database, if not add them
+        if not User.query.filter_by(id=session['uuid']).first():
+            new_user = User(id=session['uuid'], email=session['email'])
+            db.session.add(new_user)
+            db.session.commit()
 
-    # Redirect to dashboard
-    return redirect(url_for('dashboard'))
+        # Redirect to dashboard
+        return redirect(url_for('dashboard'))
+    except Exception as e:
+        return jsonify({'error': 'Login failed', 'message': str(e)}), 401
 
 # Register route
 @app.route('/register', methods=['POST'])
 def register():
-    # Extract user data from request
-    data = request.form
-    email = data['email']
-    password = data['password']
+    try:
+        # Extract user data from request
+        data = request.form
+        email = data['email']
+        password = data['password']
 
-    # Create user
-    user = auth.create_user_with_email_and_password(email, password)
+        # Create user
+        user = auth.create_user_with_email_and_password(email, password)
 
-    # Store user in session
-    session['uuid'] = user['localId']
-    session['email'] = email
-    session['token'] = user['idToken']
+        # Store user in session
+        session['uuid'] = user['localId']
+        session['email'] = email
+        session['token'] = user['idToken']
 
-    # Add user to database
-    new_user = User(id=session['uuid'], email=session['email'])
-    db.session.add(new_user)
+        # Add user to database
+        new_user = User(id=session['uuid'], email=session['email'])
+        db.session.add(new_user)
 
-    # Redirect to dashboard
-    return redirect(url_for('dashboard'))
+        # Redirect to dashboard
+        return redirect(url_for('dashboard'))
+    except Exception as e:
+        return jsonify({'error': 'Registration failed', 'message': str(e)}), 401
 
 # Add transaction route
 @app.route('/add_transaction', methods=['POST'])
 def add_transaction():
-    if 'token' not in session:
-        return redirect(url_for('loginregister'))
-    
-    # Extract transaction data from request
     try:
-        data = request.form
-        date = datetime.strptime(data['date'], '%Y-%m-%d').date()
-        name = data['name']
-        amount = float(data['amount'])
-        recurring = data.get('recurring') == 'on'
-        user_id=session['uuid']
-    except Exception as e:
-        print(e)
-        return redirect(url_for('dashboard'))
-    
-    # Add transaction to database
-    new_transaction = Transaction(date=date, name=name, amount=amount, recurring=recurring, user_id=user_id)
-    db.session.add(new_transaction)
-    db.session.commit()
+        if 'token' not in session:
+            return redirect(url_for('loginregister'))
+        
+        # Extract transaction data from request
+        try:
+            data = request.form
+            date = datetime.strptime(data['date'], '%Y-%m-%d').date()
+            name = data['name']
+            amount = float(data['amount'])
+            description = data.get('description')
+            user_id=session['uuid']
+        except Exception as e:
+            print(e)
+            return redirect(url_for('dashboard'))
+        
+        # Add transaction to database
+        new_transaction = Transaction(date=date, name=name, amount=amount, description=description, user_id=user_id)
+        db.session.add(new_transaction)
+        db.session.commit()
 
-    # Redirect to dashboard
-    return redirect(url_for('dashboard'))
+        # Redirect to dashboard
+        return redirect(url_for('dashboard'))
+    except ValueError as e:
+        return jsonify({'error': 'Invalid Input', 'message': str(e)}), 400
+    except Exception as e:
+        return jsonify({'error': 'Failed to Add Transaction', 'message': str(e)}), 500
 
 # Delete transaction route
 @app.route('/delete_transaction/<int:transaction_id>', methods=['DELETE'])
 def delete_transaction(transaction_id):
-    if 'token' not in session:
-        return redirect(url_for('loginregister'))
-    
-    # Extract transaction id from request
-    transaction = Transaction.query.filter_by(id=transaction_id).first()
-    db.session.delete(transaction)
-    db.session.commit()
+    try:
+        if 'token' not in session:
+            return redirect(url_for('loginregister'))
+        
+        # Extract transaction id from request
+        transaction = Transaction.query.filter_by(id=transaction_id).first()
+        db.session.delete(transaction)
+        db.session.commit()
 
-    return jsonify({'message': 'Transaction deleted'}), 200
+        return redirect(url_for('dashboard'))
+    except ValueError as e:
+        return jsonify({'error': 'Invalid Input', 'message': str(e)}), 400
+    except Exception as e:
+        return jsonify({'error': 'Failed to Delete Transaction', 'message': str(e)}), 500
 
 
 # Get transactions route
-@app.route('/api/get_transactions', methods=['GET'])
+@app.route('/get_transactions', methods=['GET'])
 def get_transactions():
-    if 'token' not in session:
-        return redirect(url_for('loginregister'))
-    
-    # Get transactions from database
-    transactions = Transaction.query.filter_by(user_id=session['uuid']).all()
+    try:
+        if 'token' not in session:
+            return redirect(url_for('loginregister'))
+        
+        # Get transactions from database
+        transactions = Transaction.query.filter_by(user_id=session['uuid']).all()
 
-    # Convert transactions to JSON
-    transactions_json = []
-    for transaction in transactions:
-        transactions_json.append({
-            'id': transaction.id,
-            'date': transaction.date,
-            'name': transaction.name,
-            'amount': transaction.amount,
-            'recurring': transaction.recurring
-        })
+        # Convert transactions to JSON
+        transactions_json = []
+        for transaction in transactions:
+            transactions_json.append({
+                'id': transaction.id,
+                'date': transaction.date,
+                'name': transaction.name,
+                'amount': transaction.amount,
+                'description': transaction.description
+            })
 
-    # Return transactions as JSON
-    return {'transactions': transactions_json}, 200
+        # Return transactions as JSON
+        return {'transactions': transactions_json}, 200
+    except Exception as e:
+        return jsonify({'error': 'Failed to Get Transactions', 'message': str(e)}), 500
 
 # Logout route
 @app.route('/logout', methods=['POST'])
--- src/models.py
+++ src/models.py
@@ -4,14 +4,13 @@ from extension import db
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
     amount = db.Column(db.Float, nullable=False)
-    recurring = db.Column(db.Boolean, nullable=False)
-    user_id = db.Column(db.String(127), db.ForeignKey('user.id'), nullable=False)
-
+    description = db.Column(db.String(256), nullable=False)
+    user_id = db.Column(db.String(128), db.ForeignKey('user.id'), nullable=False)
 
 # User class
 class User(db.Model):
-    id = db.Column(db.String(127), primary_key=True) # Firebase UUID
-    email = db.Column(db.String(255), nullable=False)
+    id = db.Column(db.String(128), primary_key=True) # Firebase UUID
+    email = db.Column(db.String(256), nullable=False)
     transactions = db.relationship('Transaction', backref='user', lazy=True)
